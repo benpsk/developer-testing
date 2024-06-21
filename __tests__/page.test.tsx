864bb88e76db8430d09422d93126fe0c518fe801
Ben Gunn <bengunn.dev@gmail.com>
2024-06-22 05:57:09 +0700

    test: unit test - didnt work

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,19 @@
+// __tests__/Home.test.tsx
+import { render, screen } from '@testing-library/react';
+import Home from '@/components/home';
+import { expect, test } from 'vitest'
+import ApolloProvider from '../app/ApolloProvider';
+import { initialMockData } from '@/__mocks__/handlers';
+
+
+test('renders Home component with initial data', () => {
+  render(
+    <ApolloProvider >
+      <Home initialData={initialMockData} />
+    </ApolloProvider>
+  );
+  expect(screen.getByText('Title - hello title')).toBeDefined();
+  expect(screen.getByText('Name - hello name')).toBeDefined();
+  expect(screen.getByText('RENT')).toBeDefined();
+});
+
